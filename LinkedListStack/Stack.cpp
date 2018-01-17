e41c5d2c62d908768cf63bd3fc2bed33be8a1238
Andrey Makuhin <a_makuhin@mail.ru>
2018-01-17 19:52:46 +0500

    Чо та накодал, ничо не работает, зато начало положено

--- LinkedListStack/Stack.cpp
+++ LinkedListStack/Stack.cpp
@@ -1,1 +1,87 @@
 #include "Stack.h"
+
+Stack::Stack()
+{
+	head = new Item();
+	tail = head;
+	tail->next = nullptr;
+}
+
+Stack::~Stack()
+{
+	Item* iter = head;	
+	Item* tmp = iter;
+	while (iter->next)
+	{
+		iter = iter->next;
+		delete tmp;
+		tmp = iter;
+	}
+	
+	delete iter;
+}
+
+void Stack::Push(int val)
+{
+	tail->next = new Item(val);
+	tail = tail->next;
+	tail->next = nullptr;
+	size++;
+}
+
+int Stack::Pop()
+{
+	
+	if (size > 0)
+	{
+		int val = tail->Value();
+		Item* iter = head;
+		if (size > 1)
+		{
+			for (int i = 1; i < size - 1; ++i)
+			{
+				iter = iter->next;
+			}
+		}
+
+		if (size == 1)
+		{
+			delete tail;
+			tail = head;
+			tail->next = nullptr;
+		}
+
+		delete tail;
+		tail = iter;
+
+		return val;
+	}
+	else
+	{
+		return 0;
+	}
+
+	
+	
+}
+
+int Stack::Size() const
+{
+	return size;
+}
+
+bool Stack::Empty() const
+{
+	return size == 0;
+}
+
+Stack::Item::Item(int value)
+	:
+	val(value)
+{
+}
+
+int Stack::Item::Value() const
+{
+	return val;
+}
--- LinkedListStack/Stack.h
+++ LinkedListStack/Stack.h
@@ -3,8 +3,25 @@
 class Stack
 {
 public:
+	class Item
+	{
+	public:
+		Item() = default;
+		Item(int value);
+		Item* next;
+		int Value()const;
+	private:
+		int val;
+	};
+public:
+	Stack();
+	~Stack();
 	void Push( int val );
 	int Pop();
 	int Size() const;
 	bool Empty() const;
+private:
+	int size = 0;
+	Item* head;
+	Item* tail;
 };
