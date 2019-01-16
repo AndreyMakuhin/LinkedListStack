3f95f990e08d27cbde0695a54cbd32a0e0217a2c
Андрей Макухин <a_makuhin@mail.ru>
2019-01-17 00:46:41 +0500

    Начал домашку, пока ничего путного не написал.

--- LinkedListStack/Stack.cpp
+++ LinkedListStack/Stack.cpp
@@ -1,1 +1,22 @@
 #include "Stack.h"
+
+void Stack::Push(int val)
+{
+	Element newElement(val);
+}
+
+int Stack::Size() const
+{
+	return size;
+}
+
+bool Stack::Empty() const
+{
+	return size == 0;
+}
+
+Stack::Element::Element(int val)
+	:
+	value(val)
+{
+}
--- LinkedListStack/Stack.h
+++ LinkedListStack/Stack.h
@@ -2,9 +2,27 @@
 
 class Stack
 {
+private:
+	class Element
+	{
+	public:
+		Element(int val);
+	public:		
+		int value;
+		Element* pNext;
+		Element* pPrev;
+	};
+
 public:
+	Stack() = default;
+	~Stack();
 	void Push( int val );
 	int Pop();
 	int Size() const;
 	bool Empty() const;
+
+private:
+	Element* pBottom = nullptr;
+	Element* pTop = nullptr;
+	int size = 0;
 };
